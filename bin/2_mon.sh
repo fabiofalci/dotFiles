abac1032ff4e019959820ee86460786ebb5774be
Fabio Falci <fabiofalci@gmail.com>
2019-01-15 10:38:04 +0000

    Various changes

--- bin/2_mon.sh
+++ bin/2_mon.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 1680x0 --rotate normal --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 1680x1050 --pos 0x288 --rotate normal --output HDMI2 --mode 1920x1080 --pos 1680x0 --rotate right --output HDMI1 --off --output DP2 --off
