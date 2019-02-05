3edd1020e80c598b02e5512239ea1bbee98fe8b4
Fabio Falci <fabiofalci@gmail.com>
2019-02-05 11:56:08 +0000

    Using 1 monitor for now

--- bin/laptop_monitor.sh
+++ bin/laptop_monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-xrandr \
-	--output eDP-1 --mode 2560x1600 --pos 0x0 --rotate normal \
-	--output HDMI2 --off \
-	--output VIRTUAL1 --off \
-	--output DP1 --off \
-	--output HDMI1 --off \
-	--output DP2 --off
+	xrandr \
+		--output eDP1 --mode 2560x1600 --pos 0x0 --rotate normal \
+		--output HDMI2 --off \
+		--output VIRTUAL1 --off \
+		--output DP1 --off \
+		--output HDMI1 --off \
+		--output DP2 --off
 
