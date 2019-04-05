72ed51b31a481133f65c0aa487fada470c2b9a72
Fabio Falci <fabiofalci@gmail.com>
2019-04-05 15:19:58 +0100

    Various changes

--- bin/i3.sh
+++ bin/i3.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
 i3-msg workspace 1
-i3-msg exec termite
-
-i3-msg workspace 2
 i3-msg exec "google-chrome-stable --profile-directory='Profile 1'"
 sleep 1
 i3-msg exec "google-chrome-stable --profile-directory=Default"
 i3-msg layout tabbed
 sleep 3
 
-i3-msg workspace 3
+i3-msg workspace 2
 i3-msg exec "google-chrome-stable --profile-directory=Default"
+i3-msg layout tabbed
 sleep 3
+
+i3-msg workspace 9
+i3-msg exec termite
+i3-msg layout tabbed
 
 i3-msg workspace 10
 i3-msg exec termite
 
-i3-msg workspace 5
+i3-msg workspace 4
 i3-msg exec termite
 
-i3-msg workspace 4
+i3-msg workspace 3
 i3-msg exec intellij-idea-ultimate-edition
+i3-msg layout tabbed
 
