a79c7643f75a72a5550cda0e135364626608f90c
Fabio Falci <fabiofalci@gmail.com>
2019-04-29 10:03:40 +0100

    Monitor changes, i3 changes

--- bin/1_mon.sh
+++ bin/1_mon.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --primary --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr \
+	--output VIRTUAL1 --off \
+	--output eDP1 --off \
+	--output DP1 --off \
+	--output HDMI2 --off \
+	--output HDMI1 --off \
+	--output DP2 --primary --mode 1680x1050 --pos 0x0 --rotate normal
--- bin/laptop_monitor.sh
+++ bin/laptop_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 	xrandr \
-		--output eDP1 --mode 2560x1600 --pos 0x0 --rotate normal \
+		--output eDP1 --primary --mode 2560x1600 --pos 0x0 --rotate normal \
 		--output HDMI2 --off \
 		--output VIRTUAL1 --off \
 		--output DP1 --off \
