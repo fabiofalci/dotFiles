c00d8cb1dac37d5dcf02daf2d676d4701c7ee561
Fabio Falci <fabiofalci@gmail.com>
2019-05-02 11:33:12 +0100

    Using sconsify from usr bin

--- i3/media_buttons.sh
+++ i3/media_buttons.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ "$1" == "XF86AudioPrev" ]]; then
-	/home/fabio/bin/sconsify -command replay
+	sconsify -command replay
 	echo "seek 0 1" > /home/fabio/.mplayer/pipe
 elif [[ "$1" == "XF86AudioPlay" ]]; then
-	/home/fabio/bin/sconsify -command play_pause
+	sconsify -command play_pause
 	echo "pause" > /home/fabio/.mplayer/pipe
 elif [[ "$1" == "XF86AudioNext" ]]; then
-	/home/fabio/bin/sconsify -command next
+	sconsify -command next
 	echo "pt_step 1" > /home/fabio/.mplayer/pipe
 fi
 
