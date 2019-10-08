46abecfd678b52e2458e71f1cf810555714d54c4
Fabio Falci <fabiofalci@gmail.com>
2019-10-08 17:31:57 +0000

    Move ansible to folder

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S ansible wget git --noconfirm
+sudo pacman -S ansible git --noconfirm
 
 git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
 git clone https://github.com/fabiofalci/env.git ~/env
 
-cd env
-
-ansible-playbook --ask-become-pass arch.yml
+#cd env/ansible
+#ansible-playbook --ask-become-pass arch-vm.yml
+#ansible-playbook --ask-become-pass arch-laptop.yml
