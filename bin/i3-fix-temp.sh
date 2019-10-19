e26af9b917b2ca23d5f4323025bec274f58c87cd
Fabio Falci <fabiofalci@gmail.com>
2019-10-19 20:13:01 +0100

    Fix i3 status on xps

--- bin/i3-fix-temp.sh
+++ bin/i3-fix-temp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 replace(){
-	sed s/hwmonX/hwmon$HWMON/g ~/env/i3/i3status-config.template > ~/.config/i3status/config 
+	sed s/hwmonX/hwmon$HWMON/g ~/env/i3/i3status-config-xps.template > ~/.config/i3status/config 
 }
 
 HWMON=0
