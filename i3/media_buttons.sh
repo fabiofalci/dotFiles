db8e0b18c7187833fe11bdb34ffdf37c84b1bfff
Fabio Falci <fabiofalci@gmail.com>
2021-06-14 11:09:00 +0100

    Add support for playerctl

--- i3/media_buttons.sh
+++ i3/media_buttons.sh
@@ -3,11 +3,14 @@
 if [[ "$1" == "XF86AudioPrev" ]]; then
 	sconsify -command replay
 	#echo "seek 0 1" > ~/.mplayer/pipe
+	playerctl previous
 elif [[ "$1" == "XF86AudioPlay" ]]; then
 	sconsify -command play_pause
 	#echo "pause" > ~/.mplayer/pipe
+	playerctl play-pause
 elif [[ "$1" == "XF86AudioNext" ]]; then
 	sconsify -command next
 	#echo "pt_step 1" > ~/.mplayer/pipe
+	playerctl next
 fi
 
