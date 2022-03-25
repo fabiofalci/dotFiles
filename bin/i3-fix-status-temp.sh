de529355c9cd0f9808a778abe2f805e15f8563b8
Fabio Falci <fabiofalci@gmail.com>
2022-03-25 19:41:50 +0000

    Add new fix temperature passing the machine name

---
+++ bin/i3-fix-status-temp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+replace(){
+	sed s/hwmonX/hwmon$HWMON/g ~/env/i3/i3status-config-$MACHINE.template > ~/.config/i3status/config 
+}
+
+HWMON=0
+MACHINE=$1
+
+for file in /sys/devices/platform/coretemp.0/hwmon/*; do
+	name=$(basename "$file")
+	HWMON=${name:5}
+done
+
+replace
