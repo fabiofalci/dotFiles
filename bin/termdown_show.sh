2e2c77eb44968829c54116039b5036171e92b100
Fabio Falci <fabiofalci@gmail.com>
2022-07-07 10:28:02 +0100

    Separating script

---
+++ bin/termdown_show.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+re='^[0-9]+$'
+COUNTER=0
+DAY=0
+TOTAL=0
+
+task_print_day(){
+	MIN=$(( COUNTER / 60 ))
+	OUTPUT=$( date -d@$MIN -u +%H:%M:%S )
+	printf "$DAY   $OUTPUT\n"
+}
+
+task_summary(){
+	while read -r line
+	do
+		if [ -z "${line}" ]; then
+			task_print_day
+		elif [[ $line =~ $re ]] ; then
+			COUNTER=$(( COUNTER + line ))
+			TOTAL=$(( TOTAL + line ))
+		else
+			COUNTER=0
+			DAY=$line
+		fi
+	done <<< $(calcurse -d -7 --format-apt='%d\n')
+
+	task_print_day
+	printf "Total:      %s\n" $( date -d@$TOTAL -u +%H:%M:%S )
+}
+
+task_summary
+
+
