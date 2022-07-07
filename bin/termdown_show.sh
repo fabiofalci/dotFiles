eba8a781fcc37cc6e753abb644da7f9dc47ed34a
Fabio Falci <fabiofalci@gmail.com>
2022-07-07 17:34:17 +0100

    Add number of tasks

--- bin/termdown_show.sh
+++ bin/termdown_show.sh
@@ -2,13 +2,14 @@
 
 re='^[0-9]+$'
 COUNTER=0
+TASKS=0
 DAY=0
 TOTAL=0
 
 task_print_day(){
 	MIN=$(( COUNTER / 60 ))
 	OUTPUT=$( date -d@$MIN -u +%H:%M:%S )
-	printf "$DAY   $OUTPUT\n"
+	printf "%s   %s  %s\n" $DAY $OUTPUT $TASKS
 }
 
 task_summary(){
@@ -19,8 +20,10 @@ task_summary(){
 		elif [[ $line =~ $re ]] ; then
 			COUNTER=$(( COUNTER + line ))
 			TOTAL=$(( TOTAL + line ))
+			TASKS=$(( TASKS + 1 ))
 		else
 			COUNTER=0
+			TASKS=0
 			DAY=$line
 		fi
 	done <<< $(calcurse -d -7 --format-apt='%d\n')
