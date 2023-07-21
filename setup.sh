9b302014f07fccc946ae0125a8fee315fe07324b
Fabio Falci <fabiofalci@gmail.com>
2023-07-21 12:21:22 +0100

    Tidying up ansible

--- setup.sh
+++ setup.sh
@@ -13,7 +13,25 @@ curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewl
 git clone https://github.com/fabiofalci/env.git ~/env
 
 #cd env/ansible
-#ansible-playbook --ask-become-pass arch-vm.yml
+#ansible-playbook --ask-become-pass arch-vm.yml --extra-vars "run_pacman=Y run_aur=Y ssh=Y user=fabio dotFilesPath=~/.config/dotFiles"
 #ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
 #ansible-playbook --ask-become-pass user-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
 #ansible-playbook --ask-become-pass user-install.yml --extra-vars "user=fabio profile=$HOSTNAME-ff dotFilesPath=~/.config/dotFiles"
+
+
+#when: dotFiles is defined
+#  dotFilesPath=~/.config/dotFiles"
+#when: profile is defined
+#  profile=$HOSTNAME-f
+#when: run_aur is defined
+#  run_aur=Y
+#when: run_pacman is defined
+#  run_pacman=Y
+#when: ssh is defined
+#  ssh=Y
+#when: syncthingservice is defined
+#  syncthingservice=Y
+#when: ui is defined
+#  ui=Y
+#when: user is defined
+#  user=fabio
