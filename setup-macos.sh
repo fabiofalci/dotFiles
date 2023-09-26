8ceb51337720ecf9ff25c3d8fb3474aa6e5feb0a
Fabio Falci <fabiofalci@gmail.com>
2023-09-26 16:02:14 +0100

    Fixing shell for z

--- setup-macos.sh
+++ setup-macos.sh
@@ -8,28 +8,6 @@ brew install ansible
 
 git clone https://github.com/fabiofalci/dotFiles.git ~/.config/dotFiles
 
+cd ~/.config/dotFiles/ansible-macos
 
-
-#cd env/ansible
-#ansible-playbook --ask-become-pass arch-vm.yml --extra-vars "run_pacman=Y run_aur=Y ssh=Y user=fabio dotFilesPath=~/.config/dotFiles"
-#ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
-#ansible-playbook --ask-become-pass user-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
-#ansible-playbook --ask-become-pass user-install.yml --extra-vars "user=fabio profile=$HOSTNAME-ff dotFilesPath=~/.config/dotFiles"
-
-
-#when: dotFiles is defined
-#  dotFilesPath=~/.config/dotFiles"
-#when: profile is defined
-#  profile=$HOSTNAME-f
-#when: run_aur is defined
-#  run_aur=Y
-#when: run_pacman is defined
-#  run_pacman=Y
-#when: ssh is defined
-#  ssh=Y
-#when: syncthingservice is defined
-#  syncthingservice=Y
-#when: ui is defined
-#  ui=Y
-#when: user is defined
-#  user=fabio
+ansible-playbook install.yml --extra-vars dotFilesPath=~/.config/dotFiles
