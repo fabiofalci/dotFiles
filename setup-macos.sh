068339130de99b71f9dcdc49998c3c957e63f6ca
Fabio Falci <fabiofalci@gmail.com>
2023-09-27 12:32:01 +0100

    Add private install

--- setup-macos.sh
+++ setup-macos.sh
@@ -11,3 +11,9 @@ git clone https://github.com/fabiofalci/dotFiles.git ~/.config/dotFiles
 cd ~/.config/dotFiles/ansible-macos
 
 ansible-playbook install.yml --extra-vars dotFilesPath=~/.config/dotFiles
+
+# set up ssh keys and unlock dotFiles-private
+
+git clone git@bitbucket.org:fabiofalci/dotfiles-private.git ~/.config/dotFiles-private
+
+ansible-playbook install-private.yml --extra-vars dotFilesPath=~/.config/dotFiles dotFilesPrivatePath=~/.config/dotFiles-private
