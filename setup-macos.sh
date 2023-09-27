c42787b510240c5cd6b7157a14613283d9d940ba
Fabio Falci <fabiofalci@gmail.com>
2023-09-27 21:54:28 +0100

    Separating some configuration

--- setup-macos.sh
+++ setup-macos.sh
@@ -9,9 +9,10 @@ brew install ansible
 git clone https://github.com/fabiofalci/dotFiles.git ~/.config/dotFiles
 
 cd ~/.config/dotFiles/ansible-macos
-ansible-playbook install.yml --extra-vars dotFilesPath=~/.config/dotFiles
+ansible-playbook install-conf.yml --extra-vars dotFilesPath=~/.config/dotFiles
 
 # set up ssh keys and unlock dotFiles-private
 
 git clone git@bitbucket.org:fabiofalci/dotfiles-private.git ~/.config/dotFiles-private
 ansible-playbook install-private.yml --extra-vars "dotFilesPath=~/.config/dotFiles dotFilesPrivatePath=~/.config/dotFiles-private"
+ansible-playbook install-packages.yml --extra-vars dotFilesPath=~/.config/dotFiles
