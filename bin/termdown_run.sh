a04da87317723b37ea6c5069cf3574249a70c34c
Fabio Falci <fabiofalci@gmail.com>
2023-10-03 17:31:25 +0100

    Add back linux stuff

--- bin/termdown_run.sh
+++ bin/termdown_run.sh
@@ -10,11 +10,11 @@ task=$(date "+%m/%d/%Y @ %H:%M -> %m/%d/%Y @ $1|$2")
 echo $task >> ~/.local/share/calcurse/apts
 
 termdown $1
-#notify-send "Done" $(TZ=UTC date --date now-$now +%H:%M:%S)
-#paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+notify-send "Done" $(TZ=UTC date --date now-$now +%H:%M:%S)
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 
 printf "Added task: '$task'\n"
 printf "Task total: %s\n" $(TZ=UTC date --date now-$now +%H:%M:%S)
 
-#~/.config/dotFiles/bin/termdown_show.sh
+~/.config/dotFiles/bin/termdown_show.sh
 
