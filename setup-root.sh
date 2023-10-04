b46b0882cef9bb13cd2182abb7ecc3eea5d1b749
Fabio Falci <fabiofalci@gmail.com>
2023-10-04 11:46:09 +0100

    Ignore ssh when install root

--- setup-root.sh
+++ setup-root.sh
@@ -7,4 +7,4 @@ mkdir -pv ~/.ansible/plugins/modules
 curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
 
 cd ~/.config/dotFiles/ansible
-ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y user=fabio ssh=Y dotFilesPath=~/.config/dotFiles"
+ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y user=fabio dotFilesPath=~/.config/dotFiles"
