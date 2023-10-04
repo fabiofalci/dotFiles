331b9659972a4fb6627b1a93b16b927bd04273de
Fabio Falci <fabiofalci@gmail.com>
2023-10-04 11:45:25 +0100

    Remove gradle and add needed flag to pacman

--- setup-root.sh
+++ setup-root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pacman -Syu --noconfirm
-pacman -S ansible git --noconfirm
+pacman -S --needed ansible git --noconfirm
 
 mkdir -pv ~/.ansible/plugins/modules
 curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
