ced163c30f1a1d69b310252ac05e4f192461d714
Fabio Falci <fabiofalci@gmail.com>
2023-10-04 11:50:17 +0100

    User set up

---
+++ setup-user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git clone https://github.com/fabiofalci/dotFiles.git ~/.config/dotFiles
+
+cd ~/.config/dotFiles/ansible
+ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
+ansible-playbook --ask-become-pass user-install.yml --extra-vars "run_pacman=Y run_aur=Y ui=Y user=fabio dotFilesPath=~/.config/dotFiles"
+
+#ansible-playbook --ask-become-pass arch-vm.yml --extra-vars "run_pacman=Y run_aur=Y ssh=Y user=fabio dotFilesPath=~/.config/dotFiles"
+#ansible-playbook --ask-become-pass user-install.yml --extra-vars "user=fabio profile=$HOSTNAME-ff dotFilesPath=~/.config/dotFiles"
+
+
+#when: dotFiles is defined
+#  dotFilesPath=~/.config/dotFiles"
+#when: profile is defined
+#  profile=$HOSTNAME-f
+#when: run_aur is defined
+#  run_aur=Y
+#when: run_pacman is defined
+#  run_pacman=Y
+#when: ssh is defined
+#  ssh=Y
+#when: syncthingservice is defined
+#  syncthingservice=Y
+#when: ui is defined
+#  ui=Y
+#when: user is defined
+#  user=fabio
