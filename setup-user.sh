e8586efb26987aaaf9f79a67cce93c9cf367a44f
Fabio Falci <fabiofalci@gmail.com>
2023-10-07 22:40:54 +0100

    Tidying up for linux vm

--- setup-user.sh
+++ setup-user.sh
@@ -6,7 +6,6 @@ mkdir -pv ~/.ansible/plugins/modules
 curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
 
 cd ~/.config/dotFiles/ansible
-ansible-playbook --ask-become-pass sys-install.yml --extra-vars "run_pacman=Y run_aur=Y user=fabio dotFilesPath=~/.config/dotFiles"
 ansible-playbook --ask-become-pass user-install.yml --extra-vars "run_pacman=Y run_aur=Y ui=Y user=fabio dotFilesPath=~/.config/dotFiles"
 
 #ansible-playbook --ask-become-pass arch-vm.yml --extra-vars "run_pacman=Y run_aur=Y ssh=Y user=fabio dotFilesPath=~/.config/dotFiles"
