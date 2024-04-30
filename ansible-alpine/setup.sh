159ce644ed66d631aeec0db4cc09134551629edd
Fabio Falci <fabiofalci@gmail.com>
2024-04-30 09:36:26 +0100

    Ansible configuration

--- ansible-alpine/setup.sh
+++ ansible-alpine/setup.sh
@@ -5,4 +5,5 @@
 apk add ansible
 apk add zsh
 #ansible-playbook --ask-become-pass alpine-vm.yml --extra-vars "run_pacman=Y run_aur=Y ui=Y user=fabio profile=shark dotFilesPath=~/.config/dotFiles"
+#ansible-playbook --ask-become-pass alpine-vm.yml --extra-vars "dotFilesPath=~/.config/dotFiles"
 chsh fabio
