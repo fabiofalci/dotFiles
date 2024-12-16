c65588a9778d3b9357408becef8b9a3ff7b0c49f
Fabio Falci <fabiofalci@gmail.com>
2024-12-16 08:13:37 +0000

    Config

--- i3/media_buttons.sh
+++ i3/media_buttons.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ "$1" == "XF86AudioPrev" ]]; then
-	sconsify -command replay
+	#sconsify -command replay
 	#echo "seek 0 1" > ~/.mplayer/pipe
 	playerctl previous
 elif [[ "$1" == "XF86AudioPlay" ]]; then
-	sconsify -command play_pause
+	#sconsify -command play_pause
 	#echo "pause" > ~/.mplayer/pipe
 	playerctl play-pause
 elif [[ "$1" == "XF86AudioNext" ]]; then
-	sconsify -command next
+	#sconsify -command next
 	#echo "pt_step 1" > ~/.mplayer/pipe
 	playerctl next
 fi
