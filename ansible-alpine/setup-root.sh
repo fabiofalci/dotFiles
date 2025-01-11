74c8b98c8e492c788637f0874de0ad73586a3ddc
Fabio Falci <fabiofalci@gmail.com>
2025-01-11 23:51:12 +0000

    Fix folder

--- ansible-alpine/setup-root.sh
+++ ansible-alpine/setup-root.sh
@@ -3,6 +3,5 @@
 apk upgrade
 apk add ansible
 
-cd ~/.config/dotFiles/ansible
 ansible-playbook --ask-become-pass install-user.yml --extra-vars "user=fabio ssh=Y dotFilesPath=~/.config/dotFiles"
 # ansible-playbook --ask-become-pass install-user.yml --extra-vars "user=fabio ssh=Y dotFilesPath=~/.config/dotFiles dotFilesPrivatePath=~/.config/dotFiles-private"
