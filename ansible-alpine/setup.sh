259e5de148e0e9142a40e98a8820e11881dcb4a1
Fabio Falci <fabiofalci@gmail.com>
2025-01-12 20:52:23 +0000

    Remove some commands

--- ansible-alpine/setup.sh
+++ ansible-alpine/setup.sh
@@ -2,7 +2,7 @@
 
 #git clone https://github.com/fabiofalci/dotFiles.git ~/.config/dotFiles
 
-apk add ansible
-apk add zsh
+# apk add ansible
+# apk add zsh
 ansible-playbook --ask-become-pass alpine-vm.yml --extra-vars "dotFilesPath=~/.config/dotFiles  dotFilesPrivatePath=~/.config/dotFiles-private"
 #chsh fabio
